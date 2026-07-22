0b69fca2f9b8f7fe12c6dc0883393bc25381e104
Ann Ene Agbo <agboannene@users.noreply.github.com>
2026-07-22 13:46:01 +0100

    chore: remove debug error boundaries and unused wrapper

--- src/components/daily-spend/add-spend-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-const AddSpendForm = dynamic(() => import("./add-spend-form").then(m => m.AddSpendForm), { ssr: false })
-
-export function AddSpendWrapper() {
-  return <AddSpendForm onSuccess={() => {}} />
-}
